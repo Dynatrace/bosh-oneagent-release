d8fa9ae7e5e6093112e9643d22d439180759d54a
aloismayr <alois.mayr@dynatrace.com>
2017-05-08 16:02:05 +0200

    Switch for running oneagenthelper, support for garden-runc 1.2.0 or higher

--- run.sh
+++
@@ -1,6 +0,0 @@
-bosh deployment dynatrace-oneagent-deployment-manifest.yml
-bosh -n delete deployment dynatrace-oneagent
-bosh -n delete release dynatrace-oneagent
-bosh create release
-bosh upload release
-bosh -n deploy
