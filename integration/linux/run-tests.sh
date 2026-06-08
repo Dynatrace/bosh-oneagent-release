f65db30ed28d47f1da61b8b11fb90ab89d2052fe
Andreas Drozd <andreas.drozd@dynatrace.com>
2026-06-08 14:23:55 +0200

    Add Dockerfile for Ubuntu 24.04 and update run-tests script for new image

--- integration/linux/run-tests.sh
+++ integration/linux/run-tests.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")/../.."
 
-tag_ubuntu_1404="dtbosh-ubuntu14.04"
+tag_ubuntu_2404="dtbosh-ubuntu24.04"
 tag_ubuntu_2604="dtbosh-ubuntu26.04"
 project="dtbosh_linux"
 docker_compose_opts="--project-name $project --file integration/linux/docker-compose.yml"
@@ -21,14 +21,14 @@ function fail {
 	exit 1
 }
 
-echo "Building Ubuntu 14.04 image for tests..."
-docker build --tag "$tag_ubuntu_1404" --file integration/linux/Dockerfile.Test.Ubuntu14.04 . || fail
+echo "Building Ubuntu 24.04 image for tests..."
+docker build --tag "$tag_ubuntu_2404" --file integration/linux/Dockerfile.Test.Ubuntu24.04 . || fail
 
 echo "Building Ubuntu 26.04 image for tests..."
 docker build --tag "$tag_ubuntu_2604" --file integration/linux/Dockerfile.Test.Ubuntu26.04 . || fail
 
-echo "Running tests for Ubuntu 14.04..."
-docker run --rm --env "DEPLOYMENT_MOCK_URL=http://apimock:8080" --network "${project}_default" "$tag_ubuntu_1404" || fail
+echo "Running tests for Ubuntu 24.04..."
+docker run --rm --env "DEPLOYMENT_MOCK_URL=http://apimock:8080" --network "${project}_default" "$tag_ubuntu_2404" || fail
 
 echo "Running tests for Ubuntu 26.04..."
 docker run --rm --env "DEPLOYMENT_MOCK_URL=http://apimock:8080" --network "${project}_default" "$tag_ubuntu_2604" || fail
